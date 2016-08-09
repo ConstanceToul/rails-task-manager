6e016d2a6247c700855cab36e6527b52d8fa9b51
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-09 14:47:00 +0200

    without refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#1. Get all the tasks
+  get "tasks" => "tasks#index", as: "tasks"
+  #4. Post a new task
+  post "tasks" => "tasks#create"
+
+#3. Get the form to create a new task
+  get "tasks/new" => "tasks#new", as: "new_task"
+
+#5. Get the form to edit an existing task
+  get "tasks/:id/edit" => "tasks#edit", as: "edit_task"
+
+  #2. Get a precise task
+  get "tasks/:id" => "tasks#show", as: "task"
+
+#6. Update an existing tasl
+  patch "tasks/:id" => "tasks#update"
+
+#7. Delete an existing task
+  delete "tasks/:id" => "tasks#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(title: "Faire la lessive", description: "c'est très chiant", difficulty: 3, done: false)
+Task.create!(title: "Passer l'aspirateur", description: "super super chiant", difficulty: 2, done: false)
+Task.create!(title: "Faire les courses", description: "n'en parlons pas", difficulty: 4, done: true)
+Task.create!(title: "Cuisiner", description: "presque amusant", difficulty: 2, done: true)
